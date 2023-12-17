6c99669e4aa406ea8005ae8f5cca01a226f445b1
Agnidhra <agnidhra2k4@gmail.com>
2023-12-17 19:19:20 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/filmopedia/Login.kt
@@ -0,0 +1,47 @@
+package com.example.filmopedia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import android.widget.Toast
+import com.example.filmopedia.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.registerText.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.email.text.toString()
+            val pass = binding.pass.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+                    firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, "Authentication Error", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/filmopedia/Register.kt
@@ -0,0 +1,57 @@
+package com.example.filmopedia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.filmopedia.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class Register : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signInText.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener{
+            val firstName = binding.firstName.text.toString()
+            val lastName = binding.lastName.text.toString()
+            val email = binding.email.text.toString()
+            val pass = binding.pass.text.toString()
+            val confirmPass = binding.confirmPass.text.toString()
+
+            if (firstName.isNotEmpty() && lastName.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(this, "Email format wrong", Toast.LENGTH_SHORT).show()
+                }
+                else if (pass == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val intent = Intent(this, Login::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}

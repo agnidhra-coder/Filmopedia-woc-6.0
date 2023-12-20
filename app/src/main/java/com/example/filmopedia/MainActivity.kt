fc56a6ec22014f205e48588cf8279c7c863dc34d
Agnidhra <agnidhra2k4@gmail.com>
2023-12-20 11:27:23 +0530

    After implementing bottomnav

--- app/src/main/java/com/example/filmopedia/MainActivity.kt
+++ app/src/main/java/com/example/filmopedia/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.filmopedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
